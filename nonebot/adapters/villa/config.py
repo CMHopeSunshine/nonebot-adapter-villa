1ca68ec46c5a88255c106345adb2268670397a20
CMHopeSunshine <277073121@qq.com>
2023-06-27 21:32:59 +0800

    ✨ 支持`websocket`连接

--- nonebot/adapters/villa/config.py
+++ nonebot/adapters/villa/config.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from pydantic import Extra, Field, BaseModel
 
@@ -6,7 +6,9 @@ from pydantic import Extra, Field, BaseModel
 class BotInfo(BaseModel):
     bot_id: str
     bot_secret: str
-    callback_url: str
+    callback_url: Optional[str] = None
+    ws_url: Optional[str] = None
+    ws_secret: Optional[str] = None
 
 
 class Config(BaseModel, extra=Extra.ignore):
