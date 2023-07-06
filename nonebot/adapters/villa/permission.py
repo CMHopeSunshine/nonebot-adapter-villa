61e7414bc02f340ffb9b3ffa631f4f5afb11c66b
CMHopeSunshine <277073121@qq.com>
2023-07-06 15:27:46 +0800

    ✨ 添加预设权限

---
+++ nonebot/adapters/villa/permission.py
@@ -0,0 +1,44 @@
+from typing import Union
+
+from nonebot.permission import Permission
+
+from nonebot.adapters.villa.api import RoleType
+from nonebot.adapters.villa import Bot, SendMessageEvent, AddQuickEmoticonEvent
+
+
+async def is_owner_or_admin(
+    bot: Bot, event: Union[SendMessageEvent, AddQuickEmoticonEvent]
+) -> bool:
+    user_id = event.from_user_id if isinstance(event, SendMessageEvent) else event.uid
+    user = await bot.get_member(villa_id=event.villa_id, uid=user_id)
+    return any(
+        role.role_type in (RoleType.OWNER, RoleType.ADMIN) for role in user.role_list
+    )
+
+
+OWNER_OR_ADMIN = Permission(is_owner_or_admin)
+"""别野房东或管理员权限"""
+
+
+async def is_owner(
+    bot: Bot, event: Union[SendMessageEvent, AddQuickEmoticonEvent]
+) -> bool:
+    user_id = event.from_user_id if isinstance(event, SendMessageEvent) else event.uid
+    user = await bot.get_member(villa_id=event.villa_id, uid=user_id)
+    return any(role.role_type == RoleType.OWNER for role in user.role_list)
+
+
+OWNER = Permission(is_owner)
+"""别野房东权限"""
+
+
+async def is_admin(
+    bot: Bot, event: Union[SendMessageEvent, AddQuickEmoticonEvent]
+) -> bool:
+    user_id = event.from_user_id if isinstance(event, SendMessageEvent) else event.uid
+    user = await bot.get_member(villa_id=event.villa_id, uid=user_id)
+    return any(role.role_type == RoleType.ADMIN for role in user.role_list)
+
+
+ADMIN = Permission(is_admin)
+"""管理员权限"""
