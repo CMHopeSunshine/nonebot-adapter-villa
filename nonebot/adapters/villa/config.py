504f11af04e8811abcf5f034bf19af25839a0717
惜月 <277073121@qq.com>
2023-07-18 20:08:08 +0800

    :sparkles: 自动格式化`pub_key` (#29)

--- nonebot/adapters/villa/config.py
+++ nonebot/adapters/villa/config.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from pydantic import BaseModel, Extra, Field, root_validator
+from pydantic import BaseModel, Extra, Field, root_validator, validator
 
 
 class BotInfo(BaseModel):
@@ -11,6 +11,20 @@ class BotInfo(BaseModel):
     ws_url: Optional[str] = None
     ws_secret: Optional[str] = None
     verify_event: bool = True
+
+    @validator("pub_key")
+    @classmethod
+    def format_pub_key(cls, v: str):
+        if v.startswith("-----BEGIN PUBLIC KEY-----"):
+            v = v[26:]
+        if v.endswith("-----END PUBLIC KEY-----"):
+            v = v[:-24]
+        v = v.replace(" ", "\n")
+        if v[0] != "\n":
+            v = "\n" + v
+        if v[-1] != "\n":
+            v += "\n"
+        return "-----BEGIN PUBLIC KEY-----" + v + "-----END PUBLIC KEY-----\n"
 
     # 不能同时存在 callback_url 和 ws_url
     @root_validator
