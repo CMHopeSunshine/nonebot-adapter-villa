3e3b910d8b5c4509d88c428ccbb845948a57b02d
CMHopeSunshine <277073121@qq.com>
2023-10-25 11:04:00 +0800

    :bookmark: bump version 0.7.0

--- nonebot/adapters/villa/config.py
+++ nonebot/adapters/villa/config.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from pydantic import BaseModel, Extra, Field, root_validator, validator
+from pydantic import BaseModel, Extra, Field
 
 
 class BotInfo(BaseModel):
@@ -8,32 +8,32 @@ class BotInfo(BaseModel):
     bot_secret: str
     pub_key: str
     callback_url: Optional[str] = None
-    ws_url: Optional[str] = None
-    ws_secret: Optional[str] = None
+    # ws_url: Optional[str] = None
+    # ws_secret: Optional[str] = None
     verify_event: bool = True
 
-    @validator("pub_key")
-    @classmethod
-    def format_pub_key(cls, v: str):
-        v = v.strip()
-        if v.startswith("-----BEGIN PUBLIC KEY-----"):
-            v = v[26:]
-        if v.endswith("-----END PUBLIC KEY-----"):
-            v = v[:-24]
-        v = v.replace(" ", "\n")
-        if v[0] != "\n":
-            v = "\n" + v
-        if v[-1] != "\n":
-            v += "\n"
-        return "-----BEGIN PUBLIC KEY-----" + v + "-----END PUBLIC KEY-----\n"
+    # @validator("pub_key")
+    # @classmethod
+    # def format_pub_key(cls, v: str):
+    #     v = v.strip()
+    #     if v.startswith("-----BEGIN PUBLIC KEY-----"):
+    #         v = v[26:]
+    #     if v.endswith("-----END PUBLIC KEY-----"):
+    #         v = v[:-24]
+    #     v = v.replace(" ", "\n")
+    #     if v[0] != "\n":
+    #         v = "\n" + v
+    #     if v[-1] != "\n":
+    #         v += "\n"
+    #     return "-----BEGIN PUBLIC KEY-----" + v + "-----END PUBLIC KEY-----\n"
 
-    # 不能同时存在 callback_url 和 ws_url
-    @root_validator
-    @classmethod
-    def check_url(cls, values):
-        if values.get("callback_url") and values.get("ws_url"):
-            raise ValueError("callback_url and ws_url cannot exist at the same time")
-        return values
+    # # 不能同时存在 callback_url 和 ws_url
+    # @root_validator
+    # @classmethod
+    # def check_url(cls, values):
+    #     if values.get("callback_url") and values.get("ws_url"):
+    #         raise ValueError("callback_url and ws_url cannot exist at the same time")
+    #     return values
 
 
 class Config(BaseModel, extra=Extra.ignore):
